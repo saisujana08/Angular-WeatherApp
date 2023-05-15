1357b322ee52c3aec3bab2ad701c91b023d90b34
saisujana08 <saisujana08@gmail.com>
2023-05-15 10:46:54 +0530

    update files

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -10,7 +10,7 @@ import {FormControl,FormGroup,Validators} from '@angular/forms';
 })
 
 export class cardComponent {
-  City!:String;
+  
   Temp!:String;
   Ci!:String;
   spin:boolean=true;
@@ -23,17 +23,32 @@ export class cardComponent {
   })
 
 
-  constructor(private servic: dataService){
+  constructor(private servic: dataService ){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(loc => {
+        this.servic.getLocation(loc.coords.latitude, loc.coords.longitude).subscribe(detail => {
+          this.Temp=String(detail.main.temp).split(".")[0];
+          this.Ci=detail.name;
+          this.spin=false;
+          this.error=false;
+        },err => {
+          this.spin=false;
+          this.error=true;
+        })
+      })
+    }
+
   }
 
   handleClimate() {
     this.spin=true;
-    this.servic.getWeather(this.City).subscribe(data => {
+    this.servic.getWeather(this.cityForm.value.cityName!).subscribe(data => {
       this.Temp = String(data.main.temp).split(".")[0];
        this.Ci = data.name;
        this.spin=false;
        this.error=false;
     },error => {this.error= true;this.spin=false})
   }
+
   
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -13,4 +13,9 @@ export class dataService {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=a1ac47818feac7ec283e7b680fe7e0fa`
     return this.http.get(url);
   }
+  getLocation(latitude: number, longitude: number): Observable<any>{
+    // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=a1ac47818feac7ec283e7b680fe7e0fa`
+    const url ="https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=a1ac47818feac7ec283e7b680fe7e0fa"
+    return this.http.get(url);
+  }
 }
