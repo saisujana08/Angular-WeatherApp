31ab0874a7307e48a4e32a0f52a36dbb55713421
saisujana08 <saisujana08@gmail.com>
2023-05-17 10:47:58 +0530

    final commit

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { dataService } from 'src/app/services/data.service';
-import {faSpinner} from '@fortawesome/free-solid-svg-icons';
-import {FormControl,FormGroup,Validators} from '@angular/forms';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { pluck, map } from 'rxjs'
+import { Chart } from 'chart.js'
 
 @Component({
   selector: 'app-card',
@@ -10,45 +12,120 @@ import {FormControl,FormGroup,Validators} from '@angular/forms';
 })
 
 export class cardComponent {
-  
-  Temp!:String;
-  Ci!:String;
-  spin:boolean=true;
-  faSpinner=faSpinner;
-  error:boolean=false;
 
-  cityForm= new FormGroup({
-    cityName:new FormControl("",[Validators.required, Validators.pattern('^[a-zA-Z \-\']+')])
+  Temp!: String;
+  Ci!: String;
+  spin: boolean = true;
+  faSpinner = faSpinner;
+  error: boolean = false;
+  weekTemp: number[] = [];
+  dates: number[] = [];
+
+
+
+  cityForm = new FormGroup({
+    cityName: new FormControl("", [Validators.required, Validators.pattern('[a-zA-Z]*')])
 
   })
 
+  graphDisplay(): void {
+    var myChart = new Chart("myChart", {
+      type: 'line',
+      data: {
+        datasets: [{
+          label: 'Temperature',
+          data: this.weekTemp,
+          backgroundColor: "rgb(115 185 243 / 65%)",
+          borderColor: "#007ee7",
+          fill: true,
+        }],
+        labels: this.dates
+      },
+    });
 
-  constructor(private servic: dataService ){
-    if(navigator.geolocation){
+  }
+
+
+  constructor(private servic: dataService) {
+    navigator.geolocation.watchPosition(function (position) {
+      console.log();
+    },
+      (error) => {
+        if (error.code == error.PERMISSION_DENIED) {
+          this.handleClimate("visakhapatnam")
+        }
+
+      });
+    if (navigator.geolocation) {
+
       navigator.geolocation.getCurrentPosition(loc => {
-        this.servic.getLocation(loc.coords.latitude, loc.coords.longitude).subscribe(detail => {
-          this.Temp=String(detail.main.temp).split(".")[0];
-          this.Ci=detail.name;
-          this.spin=false;
-          this.error=false;
-        },err => {
-          this.spin=false;
-          this.error=true;
+        this.servic.getLocation(loc.coords.latitude, loc.coords.longitude).subscribe(
+
+          detail => {
+            this.Temp = String(detail.main.temp).split(".")[0];
+            this.Ci = detail.name;
+            this.spin = false;
+            this.error = false;
+          }
+
+          , err => {
+            this.spin = false;
+            this.error = true;
+          },
+
+
+
+        )
+
+        this.servic.getUserLocWeeklyData(loc.coords.latitude, loc.coords.longitude).pipe(
+          pluck("data"),
+          pluck("weather"),
+        ).subscribe(detail => {
+          for (let i = 0; i < detail.length; i++) {
+            this.weekTemp.push(detail[i].maxtempC)
+            this.dates.push(detail[i].date)
+          }
+          this.graphDisplay()
+          this.weekTemp = []
+          this.dates = []
+
+
         })
-      })
+      }
+      )
     }
 
   }
 
-  handleClimate() {
-    this.spin=true;
-    this.servic.getWeather(this.cityForm.value.cityName!).subscribe(data => {
+  handleClimate(city: String) {
+    this.spin = true;
+    this.servic.getWeather(city).subscribe(data => {
       this.Temp = String(data.main.temp).split(".")[0];
-       this.Ci = data.name;
-       this.spin=false;
-       this.error=false;
-    },error => {this.error= true;this.spin=false})
+      this.Ci = data.name;
+      this.spin = false;
+      this.error = false;
+    }, error => { this.error = true; this.spin = false })
+
+    this.servic.getWeeklyData(city).pipe(
+      pluck("data"),
+      pluck("weather"),
+
+    ).subscribe(weekData => {
+      for (let i = 0; i < weekData.length; i++) {
+        this.weekTemp.push(weekData[i].maxtempC)
+        this.dates.push(weekData[i].date)
+      }
+      console.log(this.weekTemp)
+      if (!this.error) {
+        this.graphDisplay()
+      }
+      this.weekTemp = []
+      this.dates = []
+
+
+    })
+
   }
 
-  
+
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -18,4 +18,15 @@ export class dataService {
     const url ="https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=a1ac47818feac7ec283e7b680fe7e0fa"
     return this.http.get(url);
   }
+  getWeeklyData(city:String): Observable<any>{
+    const url = `https://api.worldweatheronline.com/premium/v1/weather.ashx?key=d4c6ca7fdf1c4c9e9af60916231505&q=${city}&num_of_days=7&format=json`
+    return this.http.get(url);
+  }
+  getUserLocWeeklyData(latitude: number, longitude: number): Observable<any>{
+    const url = `https://api.worldweatheronline.com/premium/v1/weather.ashx?key=d4c6ca7fdf1c4c9e9af60916231505&q=${latitude},${longitude}&num_of_days=7&format=json`
+    return this.http.get(url);
+  }
 }
+
+
+
